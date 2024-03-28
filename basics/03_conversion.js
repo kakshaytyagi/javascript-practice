084a3d4f5a2a7e6b7a5a8d33d830f5a6cab7c591
Akshay Tyagi <akshay.tyagi@s.amity.edu>
2024-03-28 18:09:43 +0000

    conversion

---
+++ basics/03_conversion.js
@@ -0,0 +1,17 @@
+let element = 0;
+
+let bool = Boolean(element);
+console.log(bool)
+
+let element2 = null;
+let number = String(element2);
+console.log(number);
+
+// false => 0 else true => any number except 0
+// "" => 0 => false => null
+// "string" => 1 => true
+// undefined (number) => NaN 
+// undefined (boolean) => false
+// undefined (String) => undefined
+
+
